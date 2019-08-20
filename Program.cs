692b283678c71e428aab7820bc49849205edad82
Rafael Mippo <rkmippo@gmail.com>
2019-08-20 15:57:08 -0300

    Colors to the pecas was added

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace console
             {
                 Board board = new Board(8, 8);
                 board.putPeca(new King(board, Color.black), new Position(0,0));
+                board.putPeca(new Tower(board, Color.white), new Position(1,0));
 
                 Screen.printBoard(board);
             }
--- Tela.cs
+++ Tela.cs
@@ -9,15 +9,34 @@ namespace console
         {
             for(int i = 0; i < board.line; i++)
             {
+                Console.Write(8-i + " ");
                 for(int j = 0; j < board.col; j++)
                 {
                     if(board.peca(i,j) == null)
                         Console.Write("- ");
                     else
-                        Console.Write(board.peca(i,j) + " ");
+                    {
+                        printPeca(board.peca(i, j));
+                        Console.Write(" ");
+                    }
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void printPeca(Peca p)
+        {
+            if(p.color == Color.white)
+                Console.Write(p);
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(p);
+                Console.ForegroundColor = aux;
+            }
+
         }
     }
 }
